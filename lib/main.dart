c211af8545da683af3faf96d4e8d3fec9ecf49df
Yasmin <yasmin.cruz.2025072@my.istec.pt>
2026-03-24 17:10:47 +0000

    traducao da mensagem do contador para portugues

--- lib/main.dart
+++ lib/main.dart
@@ -86,6 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
+
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
@@ -104,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('Carregaste no botão este número de vezes:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
